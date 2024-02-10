b7ca6166308a56dc2d60886708c133a94cba4dc3
Rayivh <rayivh@gmail.com>
2024-02-09 23:09:37 -0800

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import {Routes, Route, Link, BrowserRouter} from 'react-router-dom';
+import Home from './components/Home.jsx'
+import Layout from "./components/Layout.jsx";
+import TestLogin from "./components/TestLogin.jsx";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+        <Layout>
+            <Routes>
+                <Route path='/' element={<Home/>} />
+                <Route path='/login' element={<TestLogin/>} />
+                <Route path='*' element={<h1>Page not found</h1>} />
+            </Routes>
+        </Layout>
+    </BrowserRouter>
   );
 }
 
